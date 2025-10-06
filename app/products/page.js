7f3a13dd4ffc41354b44f129199fdc60ac3dd875
suryaprakash8870 <71226572+suryaprakash8870@users.noreply.github.com>
2025-10-06 18:08:47 +0530

    Update page.js

--- app/products/page.js
+++ app/products/page.js
@@ -1,6 +1,6 @@
 'use client';
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+export const revalidate = false;
 import { Suspense } from 'react';
 
 import { useState, useEffect } from 'react';
